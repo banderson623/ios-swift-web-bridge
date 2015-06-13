feb55e8bbe3883f28938836da746ba5fe8111139
Brian Anderson <banderson623@gmail.com>
2015-06-13 07:38:22 -0500

    Initial Commit

---
+++ JamSlam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JamSlam
+//
+//  Created by Brian Anderson on 6/13/15.
+//  Copyright (c) 2015 Brian Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
